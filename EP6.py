eb6a31ea69a9675056cfaadd4f560546a178d93e
leonardomc3 <36934500+leonardomc3@users.noreply.github.com>
2018-04-26 10:29:37 -0300

    Update EP6.py

--- EP6.py
+++ EP6.py
@@ -27,20 +27,20 @@ while i!=0:
         1 - Acessar uma loja 
         2 - Adicionar loja 
         3 - Excluir loja''')
-    y=int(input('Faca sua escolha: '))
-    if y==0:
-        i=0
-    elif y==1:
+    y = int(input('Faca sua escolha: '))
+    if y == 0:
+        i = 0
+    elif y == 1:
         print('Lojas disponiveis: ')
         for k in Estoque:
             print(k)
-        loja=input('Qual loja deseja acessar? ')
-        loja=loja.lower()
+        loja = input('Qual loja deseja acessar? ')
+        loja = loja.lower()
         if loja not in Estoque:
             print('Loja indisponivel')
         else:
-            i=1
-            while i!=0:
+            i = 1
+            while i != 0:
                 print('''Controle de estoque da loja {0}:
                       0 - sair
                       1 - adicionar item
@@ -50,82 +50,82 @@ while i!=0:
                       5 - modificar preco
                       6 - imprimir estoque negativo
                       7 - imprimir o valor monetario'''.format(loja))
-                x=int(input('Faça sua escolha: '))
-                if x==0:
+                x = int(input('Faça sua escolha: '))
+                if x == 0:
                     print('Até mais!')    
-                    i=0
-                elif x==1:
-                    item=input('Nome do produto: ')
-                    item=item.lower()
+                    i = 0
+                elif x == 1:
+                    item = input('Nome do produto: ')
+                    item = item.lower()
                     if item in Estoque[loja]:
                         print('Produto já está cadastrado.')
                     else:
-                        quant=int(input('Quantidade inicial: '))
+                        quant = int(input('Quantidade inicial: '))
                         while quant<0:
                             print('A quantidade inicial não pode ser negativa.')
-                            quant=int(input('Quantidade iniciail: '))
-                        preco=float(input('Preco do produto: '))
+                            quant = int(input('Quantidade iniciail: '))
+                        preco = float(input('Preco do produto: '))
                         while preco<0:
                             print('O preco não pode ser negativo.')
-                            preco=float(input('Preco do produto: '))
-                        Estoque[loja][item]={'Quantidade':quant}
-                        Estoque[loja][item]['Preco']=preco
-                elif x==2:
-                    item=input('Nome do produto: ')
-                    item=item.lower()
+                            preco = float(input('Preco do produto: '))
+                        Estoque[loja][item] = {'Quantidade':quant}
+                        Estoque[loja][item]['Preco'] = preco
+                elif x == 2:
+                    item = input('Nome do produto: ')
+                    item = item.lower()
                     if item in Estoque[loja]:
                         del Estoque[loja][item]
                     else:
                         print('Elemento não encontrado')
-                elif x==3:
-                    item=input('Nome do produto: ')
-                    item=item.lower()
+                elif x == 3:
+                    item = input('Nome do produto: ')
+                    item = item.lower()
                     if item in Estoque[loja]:
-                        quant=int(input('Quantidade: '))
-                        Estoque[loja][item]['Quantidade']+=quant
+                        quant = int(input('Quantidade: '))
+                        Estoque[loja][item]['Quantidade'] += quant
                     else:
                         print('Elemento não encontrado')
-                elif x==4:
+                elif x == 4:
                     for k in Estoque[loja]:
                         print('Produto {0}: (Quantidade: {1}, Preco: {2})'.format(k,Estoque[loja][k]['Quantidade'],Estoque[loja][k]['Preco']))
-                elif x==5:
-                    item=input('Nome do produto: ')
-                    item=item.lower()
+                elif x == 5:
+                    item = input('Nome do produto: ')
+                    item = item.lower()
                     if item in Estoque[loja]:
-                        novo_preco=float(input("Digite o novo preco:"))
+                        novo_preco = float(input("Digite o novo preco:"))
                         while novo_preco <0:
                             print('O preco não pode ser negativo.')
-                            novo_preco=float(input("Digite o novo preco:"))
-                        Estoque[loja][item]["Preco"]=novo_preco
+                            novo_preco = float(input("Digite o novo preco:"))
+                        Estoque[loja][item]["Preco"] = novo_preco
                     else:
                         print("Produto nao esta no estoque")
-                elif x==6:
+                elif x == 6:
                     for produto in Estoque[loja]:
-                        if Estoque[loja][produto]['Quantidade']<0:
+                        if Estoque[loja][produto]['Quantidade'] < 0:
                             print('Produto {0}: (Quantidade: {1}, Preco: {2})'.format(produto,Estoque[loja][produto]['Quantidade'],Estoque[loja][produto]['Preco']))
-                elif x==7:
-                    somap=0
-                    soman=0
+                elif x == 7:
+                    somap = 0
+                    soman = 0
                     for produto in Estoque[loja]:
-                        if Estoque[loja][produto]['Quantidade']>0:
-                            somap+=Estoque[loja][produto]['Quantidade']*Estoque[loja][produto]['Preco']
-                        elif Estoque[loja][produto]['Quantidade']<0:
-                            soman+=Estoque[loja][produto]['Quantidade']*Estoque[loja][produto]['Preco']
-                    soman=round(soman,2)
-                    somap=round(somap,2)
-                    somat=round(somap+soman,2)
+                        if Estoque[loja][produto]['Quantidade'] > 0:
+                            somap += Estoque[loja][produto]['Quantidade'] * Estoque[loja][produto]['Preco']
+                        elif Estoque[loja][produto]['Quantidade'] < 0:
+                            soman += Estoque[loja][produto]['Quantidade']*Estoque[loja][produto]['Preco']
+                    soman = round(soman,2)
+                    somap = round(somap,2)
+                    somat = round(somap+soman,2)
                     print('Valor do Estoque bruto: {0}'.format(somap))
                     print('Valor em debito: {0}'.format(soman))
                     print('Valor total liquido:{0}'.format(somat))
     
-    elif y==2:
-        loja=input('Qual o nome da loja a adicionar? ')
+    elif y == 2:
+        loja = input('Qual o nome da loja a adicionar? ')
         if loja in Estoque:
             print('Loja já existente')
         else:
-            Estoque[loja]={}
-    elif y==3:
-        loja=input('Qual loja gostaria de excluir? ')
+            Estoque[loja] = {}
+    elif y == 3:
+        loja = input('Qual loja gostaria de excluir? ')
         if loja in Estoque:
             del Estoque[loja]
             print('Loja {0} Excluida'.format(loja))
