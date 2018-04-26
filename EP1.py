c1db7e54a17673584ba9358e031295276ee3b074
leonardomc3 <36934500+leonardomc3@users.noreply.github.com>
2018-04-26 10:34:17 -0300

    Update EP1.py

--- EP1.py
+++ EP1.py
@@ -16,41 +16,41 @@ print('''Controle de estoque
 2 - remover item
 3 - alterar item
 4 - imprimir estoque''')
-i=1
-while i!=0:
-    x=int(input('Faça sua escolha: '))
-    if x==0:
+i = 1
+while i != 0:
+    x = int(input('Faça sua escolha: '))
+    if x == 0:
         print('Até mais!')    
-        i=0
-    elif x==1:
-        item=input('Nome do produto: ')
+        i = 0
+    elif x == 1:
+        item = input('Nome do produto: ')
         if item in Estoque:
             print('Produto já está cadastrado.')
         else:
-            quant=int(input('Quantidade inicial: '))
-            while quant<0:
+            quant = int(input('Quantidade inicial: '))
+            while quant < 0:
                 print('A quantidade inicial não pode ser negativa.')
-                quant=int(input('Quantidade iniciail: '))
-            preco=float(input('Preco do produto: '))
+                quant = int(input('Quantidade iniciail: '))
+            preco = float(input('Preco do produto: '))
             while preco<0:
                 print('O preco não pode ser negativo.')
-                preco=float(input('Preco do produto: '))
-            Estoque[item]={'Quantidade':quant}
+                preco = float(input('Preco do produto: '))
+            Estoque[item] = {'Quantidade':quant}
             Estoque[item]['Preco']=preco
-    elif x==2:
-        item=input('Nome do produto: ')
+    elif x == 2:
+        item = input('Nome do produto: ')
         if item in Estoque:
             del Estoque[item]
         else:
             print('Elemento não encontrado')
-    elif x==3:
-        item=input('Nome do produto: ')
+    elif x == 3:
+        item = input('Nome do produto: ')
         if item in Estoque:
-            quant=int(input('Quantidade: '))
-            Estoque[item]['Quantidade']+=quant
+            quant = int(input('Quantidade: '))
+            Estoque[item]['Quantidade'] += quant
         else:
             print('Elemento não encontrado')
-    elif x==4:
+    elif x == 4:
         for k in Estoque:
             print('{0}:{1}'.format(k,Estoque[k]['Quantidade']))
             
